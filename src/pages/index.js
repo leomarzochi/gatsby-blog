bd2fd2124c0df2262276c069ae113b8784c35cef
Leo Marzochi <leomarzochi@live.com>
2020-02-25 22:01:17 -0300

    adicionado imagem no card de posts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { graphql, StaticQuery } from "gatsby"
+import { Row, Col } from "reactstrap"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -9,22 +10,32 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>home page</h1>
-    <StaticQuery
-      query={indexQuery}
-      render={data => (
-        <div>
-          {data.allMarkdownRemark.edges.map(({ node }) => (
-            <Post
-              title={node.frontmatter.title}
-              author={node.frontmatter.author}
-              path={node.frontmatter.path}
-              date={node.frontmatter.date}
-              body={node.excerpt}
-            />
-          ))}
-        </div>
-      )}
-    ></StaticQuery>
+    <Row>
+      <Col md="8">
+        <StaticQuery
+          query={indexQuery}
+          render={data => (
+            <div>
+              {data.allMarkdownRemark.edges.map(({ node }) => (
+                <Post
+                  title={node.frontmatter.title}
+                  author={node.frontmatter.author}
+                  path={node.frontmatter.path}
+                  date={node.frontmatter.date}
+                  body={node.excerpt}
+                  fluid={node.frontmatter.image.childImageSharp.fluid}
+                />
+              ))}
+            </div>
+          )}
+        ></StaticQuery>
+      </Col>
+      <Col md="4">
+        <div
+          style={{ width: "100%", height: "100%", background: "#ccc" }}
+        ></div>
+      </Col>
+    </Row>
   </Layout>
 )
 
@@ -39,6 +50,13 @@ const indexQuery = graphql`
             date(formatString: "DD/MM/YYYY")
             title
             path
+            image {
+              childImageSharp {
+                fluid(maxWidth: 1200) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
           }
           excerpt
         }
